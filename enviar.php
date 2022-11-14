2dab56e2cc6f67ea0efcc6044fa4ab50f456b9c6
Abel Diaz <abeldiaz2307@gmail.com>
2022-11-14 12:51:30 -0600

    Portafolio con php

---
+++ enviar.php
@@ -0,0 +1,23 @@
+<?php
+
+$nombre = $_POST['nombre'];
+$correo  = $_POST['correo'];
+$tema = $_POST['asunto'];
+$mensaje = $_POST['mensaje'];
+
+//Info email
+$destinario = "abeldiaz2307@gmail.com";
+$asunto = "Contacto desde portafolio";
+
+$carta = "De: $nombre \n";
+$carta .= "Correo: $correo \n";
+$carta .= "Asunto: $asunto \n";
+$carta .= "Mensaje $mensaje \n";
+$carta .= "Enviado el: " . date('d/m/Y', time());
+
+
+//Send msj
+mail($destinario, $asunto, utf8_decode($carta), $header);
+header('Location:index.html');
+
+?>
